c0957e954a83e7a43fed4244fa59eb4b556f44c0
225junior <225juniorbrindou@gmail.com>
2023-01-28 13:27:35 +0000

    ajout de route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::group(['namespace' => 'App\Http\Controllers'], function () {
+    Route::GET('/', 'ContactController@index')->name('contact.index');
+    Route::POST('/', 'ContactController@store')->name('contact.store');
 });
